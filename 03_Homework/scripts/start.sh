21e3790c24e794ceee59181fe395932a59ba42d3
Elvis TOGBAN <togbanelvis@yahoo.fr>
2024-07-25 07:58:09 -0400

    update

--- 03_Homework/scripts/start.sh
+++ 03_Homework/scripts/start.sh
@@ -2,6 +2,6 @@
 
 PROJECT_NAME=mlops \
   MAGE_CODE_PATH=/home/src \
-  SMTP_EMAIL=yodensan@gmail.com \
-  SMTP_PASSWORD=ubybegxiaeutqkrd \
+  SMTP_EMAIL=your_email \
+  SMTP_PASSWORD=your_password \
   docker compose up
